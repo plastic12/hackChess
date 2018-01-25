8112b5f68bedae47fa86ef3b32055944dd8ebc3f
plastic12 <choihogwang@gmail.com>
2018-01-25 14:07:53 +0900

    init

---
+++ src/hackChess/Chess.java
@@ -0,0 +1,16 @@
+package hackChess;
+
+public class Chess 
+{
+	private int x;
+	private int y;
+	public Chess()
+	{
+		
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("I am the God");
+	}
+
+}
